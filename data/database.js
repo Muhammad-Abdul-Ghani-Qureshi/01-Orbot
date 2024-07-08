9c327fd7ca2425c530312e983b2094d1e98fc1cd
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2024-07-08 13:23:06 +0500

    Con

--- data/database.js
+++ data/database.js
@@ -1,20 +1,26 @@
-const mongodb = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-const MongoClient = mongodb.MongoClient;
 let database;
-async function connect() {
-    const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
-    database = client.db('orbot');
+
+async function connectToDatabase() {
+  try {
+    const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+    console.log("Connected to MongoDB");
+    database = client.db("orbot");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
   }
-  
-  function getDb() {
-    if (!database) {
-      throw { message: 'Database connection not established!' };
-    }
-    return database;
+}
+
+function getDb() {
+  if (!database) {
+    throw new Error("Database connection not established!");
   }
-  
-  module.exports = {
-    connectToDatabase: connect,
-    getDb: getDb
-  };
+  return database;
+}
+
+module.exports = {
+  connectToDatabase,
+  getDb,
+};
